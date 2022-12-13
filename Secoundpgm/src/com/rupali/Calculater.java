cba5eba36232669e40fcd3a571177605cc7447ca
Rupali <rupalibhore215@gmail.com>
2022-12-13 22:01:28 +0530

    first commit

---
+++ Secoundpgm/src/com/rupali/Calculater.java
@@ -0,0 +1,15 @@
+package com.rupali;
+
+public class Calculater {
+	static int a=10,b=30;  
+	public static void add() {  //static method aslyamule variable a and b static kele ahe
+		
+		System.out.println(10+20+a+"c"+10+2); 
+	}
+	
+	public static void main(String[] args) {
+		
+		add();
+	}
+
+}
